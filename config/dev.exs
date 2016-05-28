9bb4b080acf009353c1e1594ad3e3ae12e462923
Sarat Chandra <saratchandra@outlook.com>
2016-05-28 23:32:36 +0530

    Divide config.exs

---
+++ config/dev.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+config :postman, Postman.Mailer,
+  adapter: Bamboo.LocalAdapter,
+  api_key: System.get_env("MAILGUN_API_KEY"),
+  domain: System.get_env("MAIL_DOMAIN")
+
+config :postman,
+  purpose: :email,
+  interaction: :rabbitmq,
+  from_addr: "info@skorpion.tech",
+  port: 9090
