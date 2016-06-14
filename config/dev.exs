a0e3dd19da4b49ca5539a5d5fb87e3c7575d9eaa
Sarat Chandra <saratchandra@outlook.com>
2016-06-14 12:43:31 +0530

    Remove mailgun api and domain keys from config as we are using LocalAdapter

--- config/dev.exs
+++ config/dev.exs
@@ -1,12 +1,10 @@
 use Mix.Config
 
 config :postman, Postman.Mailer,
-  adapter: Bamboo.LocalAdapter,
-  api_key: System.get_env("MAILGUN_API_KEY"),
-  domain: System.get_env("MAIL_DOMAIN")
+  adapter: Bamboo.LocalAdapter
 
 config :postman,
   purpose: :email,
-  interaction: [:api, :rabbitmq],
+  interaction: [:api],
   from_addr: "info@skorpion.tech",
   port: 9090
