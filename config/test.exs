5747e71a702bbca47bea271a73e52264d0d9351c
Sarat Chandra <saratchandra@outlook.com>
2016-06-18 00:57:38 +0530

    Add travis ci file

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,4 @@ config :postman,
   interaction: [:api, :rabbitmq],
   rabbitmq_pool_size: 1,
   from_addr: "me@saratchandra.in",
-  port: 9090,
-  rabbitmq_username: "user",
-  rabbitmq_password: "password",
-  rabbitmq_host: "rabbitmq"
+  port: 9090
